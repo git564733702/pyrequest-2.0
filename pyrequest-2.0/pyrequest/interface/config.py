2a67b5131eefa1db582dc12353403100a662d9a9
git564733702 <48816144+git564733702@users.noreply.github.com>
2019-04-15 13:51:01 +0800

    Add files via upload

---
+++ pyrequest-2.0/pyrequest/interface/config.py
@@ -0,0 +1,6 @@
+class url01() :
+    add_event="http://127.0.0.1:8000/api/add_event/"
+    add_guest="http://127.0.0.1:8000/api/add_guest/"
+    get_event_list="http://127.0.0.1:8000/api/get_event_list/"
+    get_guest_list="http://127.0.0.1:8000/api/get_guest_list/"
+    user_sign="http://127.0.0.1:8000/api/user_sign/"
